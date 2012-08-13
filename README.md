93535559bba204ae506f41a76575c48ca3dee79e
Akash Badshah <akashbad4123@gmail.com>
2012-08-13 13:32:48 +0200

    made the ror app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Party Preference Assigner
+
+This application is to be integrated with the rest of the nujabez project. Specifically this app builds up the functionality required in order to algorithmically assign party jobs to different brothers.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartyJobs::Application.config.secret_token = '157a00e5093c94384db61daa85aed6b4315d641274514d359bf8c34ac316fca6f333b8d09ce0fb6426c8841fa8edfe90294152872fe6d7e43b4d98527d857416'
